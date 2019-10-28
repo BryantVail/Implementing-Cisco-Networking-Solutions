116addf3130422cac8261da526155459c039e480
Bryant Vail <BryantVail@gmail.com>
2019-10-28 17:39:13 -0400

    osi reference model

---
+++ notes.js
@@ -0,0 +1,446 @@
+const notes = {
+  title: "Implementing Cisco Networking Solutions",
+
+  chapters: [
+    {
+      number: 1,
+      name: "Network Building Essentials",
+      learningObjectives: [
+        "The need for networking and an intro to IT Networks",
+        "Standard Reference Model for a network called: 'Open Systems Interconnection (OSI) model",
+        "TCP/IP Protocol Stack",
+        "Various stages in building a network"
+      ],
+      desc: "",
+      sections: [
+        {
+          name: "introduction to networks",
+          desc: "",
+          notes: [
+            "computers would not be as useful if they were 'egoist' or 'islands of excellence'"
+          ],
+          aspects: [
+            {
+              name: "early networks disperate protocols",
+              desc: "early computer networks used mostly proprietary protocols",
+              protocols: ["DECnet", "SNA", "IPX"],
+              notes: [
+                "limited interconnectivity due to proprietary network protocols"
+              ]
+            },
+            {
+              name: "TCP/IP",
+              desc: "Protocol that 'won'; ARPANET's internet protocol",
+              notes: ["changed computer networking"]
+            }
+          ]
+        }, //end introduction to networks
+        {
+          name: "The OSI Model and the TCP/IP Stack",
+          desc: "",
+          notes: ["OSI is only a reference model  & does not define protocols"],
+          aspects: [
+            {
+              name: "Basic Reference Model for Open Systems Interconnections",
+              abbreviation: "OSI",
+              desc:
+                "The layered model promotes grouping similar functions within a single layer & provides standard interfaces allowing the layers to communicate to another.",
+              notes: [""],
+              developers: [
+                {
+                  name: "International Organization for Standardization",
+                  abbreviation: "ISO"
+                },
+                {
+                  name:
+                    "International Telegraph and Telephone Consultative Committee",
+                  abbreviation: "CCITT",
+                  desc: "",
+                  notes: [
+                    "CCITT is abbreviated from the 'Comite Consultatif International Telephonique et Telegraphique' which later became the ITU-T",
+                    ""
+                  ]
+                }
+              ],
+              layers: [
+                {
+                  number: 1,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 2,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 3,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 4,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 5,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 6,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                },
+                {
+                  number: 7,
+                  name: "",
+                  desc: "",
+                  notes: [""]
+                }
+              ]
+            },
+            {
+              name: "ISO/IEC 7498-1 standard",
+              desc: "",
+              notes: [""],
+              components: [
+                {
+                  name: "Seven Layer Model/Basic Reference Model",
+                  desc: "an abstract model of networking "
+                },
+                {
+                  name: "A Set of specific protocols defined by other specs",
+                  desc: ""
+                }
+              ]
+            }, //end ISO/IEC 7498-1 standard
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 2,
+      name: "Networks for Digital Enterprises",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 3,
+      name: "Components of the Enterprise Network",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 4,
+      name: "Understanding and Configuring Campus Network Technologies",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 5,
+      name: "Understanding and Configuring Wireless Access Technologies",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 6,
+      name: "Understanding and Configuring WAN Technologies",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 7,
+      name: "Understanding and Configuring Data Center Technologies",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 8,
+      name: "Understanding and Configuring Network Security",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 9,
+      name: "Understanding and Configuring Quality of Service",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 10,
+      name: "A Systematic Approach to Network Management",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    }, //end chapter
+    {
+      number: 11,
+      name: "Basic Troubleshooting Skills and Techniques",
+      learningObjectives: [""],
+      desc: "",
+      sections: [
+        {
+          name: "",
+          desc: "",
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            },
+            {
+              name: "",
+              desc: "",
+              notes: [""]
+            }
+          ]
+        }
+      ] //end sections of chapter
+    } //end chapter
+  ]
+};
+
+/*
+        //section
+        {
+          name: "",
+          desc: "",
+          notes: [
+            "",
+          ],
+          aspects: [
+            {
+              name: "",
+              desc: "",
+              notes: [
+                "",
+              ]
+            },
+          ]
+        },
+
+*/
